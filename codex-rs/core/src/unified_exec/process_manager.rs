e1912003ee12f294550d63b2f0c349913e612536
Friel <friel@openai.com>
2026-08-18 23:45:55 +0000

    Retire idle environment network proxies
    Combine the existing implementation and corrections:
    
    - Recycle idle environment network proxies
    
    Original-commit: dad18539ca56d0802a9e64db0b751cfda288a07b

--- codex-rs/core/src/unified_exec/process_manager.rs
+++ codex-rs/core/src/unified_exec/process_manager.rs
@@ -70,6 +70,7 @@ use codex_core_plugins::PLUGIN_METRICS_OUTPUT_ENV_VAR;
 use codex_core_plugins::PluginCommandAttribution;
 use codex_core_plugins::PluginMetricsSidecar;
 use codex_core_plugins::strip_output_env;
+use codex_network_proxy::EnvironmentProxyLease;
 use codex_network_proxy::NetworkPolicyDecider;
 use codex_network_proxy::NetworkProxy;
 use codex_protocol::config_types::ShellEnvironmentPolicy;
@@ -505,6 +506,7 @@ impl UnifiedExecProcessManager {
             process,
             metrics_sidecar,
             permissions,
+            environment_proxy_lease,
         } = attempt;
         let process = Arc::new(process);
         if let Some(completion) = completion.as_ref() {
@@ -575,6 +577,7 @@ impl UnifiedExecProcessManager {
                 deferred_network_approval.clone(),
                 network_denial_monitor,
                 metrics_sidecar,
+                environment_proxy_lease,
                 Arc::clone(&transcript),
                 Arc::clone(&initial_exec_command_active),
             )
@@ -1123,6 +1126,7 @@ impl UnifiedExecProcessManager {
         network_approval: Option<DeferredNetworkApproval>,
         network_denial_monitor: Option<tokio::task::JoinHandle<()>>,
         metrics_sidecar: Option<PluginMetricsSidecar>,
+        environment_proxy_lease: Option<EnvironmentProxyLease>,
         transcript: Arc<tokio::sync::Mutex<HeadTailBuffer>>,
         initial_exec_command_active: Arc<AtomicBool>,
     ) {
@@ -1169,6 +1173,7 @@ impl UnifiedExecProcessManager {
             started_at,
             network_denial_monitor,
             plugin_metrics_sidecar,
+            environment_proxy_lease,
         );
     }
 
